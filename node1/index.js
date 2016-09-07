0806b169aa9fd2eb23782d5dfdb5259508035977
Stephen Michelini <sdmichelini@wpi.edu>
2016-09-06 21:01:54 -0400

    Added Content

---
+++ node1/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var path = require('path');
+
+app.get('/', function (req, res) {
+  res.type('html');
+  res.sendFile(path.join(__dirname + '/article.html'));
+});
+
+app.get('/style.css', function(req,res) {
+  res.type('css');
+  res.sendFile(path.join(__dirname + '/style.css'));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
